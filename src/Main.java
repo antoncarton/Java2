dda7a39cf998114e1885a1a60062a9980fb114c4
antoncarton <aksalutly@gmail.com>
2022-06-15 12:16:02 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 5000;
+        int rublesPerMiles = 100;
+        int bonusMiles = ticket / rublesPerMiles;
+
+        System.out.println(bonusMiles);
+    }
+}
